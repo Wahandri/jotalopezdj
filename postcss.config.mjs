3dafcfd1545e863e3e242081baa659afa981da3b
Wahandri <wahandricode@gmail.com>
2025-06-20 04:33:37 +0200

    Agregar autoprefixer como dependencia y configurar postcss para su uso

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  
+module.exports = {
+  plugins: {
+    // Puedes dejar esto vacío o agregar plugins básicos si es necesario
+    'autoprefixer': {}, // Opcional pero recomendado para compatibilidad con navegadores
+  },
 };
-
-export default config;
